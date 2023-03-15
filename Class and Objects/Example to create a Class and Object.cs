016c75917cde93ea8b6202488941dbad34a6ce45
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-15 11:19:39 +0530

    Example

---
+++ Class and Objects/Example to create a Class and Object.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Objects
+{
+    internal class Class_and_Object
+    {
+        static void Main(string[] args)
+        {
+            // creating object
+            Calculator calObject = new Calculator();
+            //Accessing Calculator class member using Calculator class object
+            int result = calObject.CalculateSum(10, 20);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        //Defining class or blueprint or template
+        public class Calculator
+        {
+            public int CalculateSum(int x, int y)
+            {
+                return x + y;
+            }
+        }
+    }
+}
