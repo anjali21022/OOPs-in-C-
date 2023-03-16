79d357fe99ac53f6d97c4b22f91b86312bcdfc24
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-16 10:46:03 +0530

    Example1

---
+++ Constructors/Example to understand Explicitly Constructors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Explicitly_Constructors
+    {
+        static void Main(string[] args)
+        {
+            ExplicitConstructor obj = new ExplicitConstructor();
+            Console.ReadKey();
+        }
+    }
+    class ExplicitConstructor
+    {
+        public ExplicitConstructor()
+        {
+            Console.WriteLine("Explicit Constructor is Called");
+        }
+    }
+}
