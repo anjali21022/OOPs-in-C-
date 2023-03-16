463da571f95061351a088fcbc3ebe65375ff1f9d
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-16 10:46:35 +0530

    Example2

---
+++ Constructors/Example to Understand Implicitly Constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Implicitly_Constructor
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            Console.WriteLine($"i = {obj.i}");
+            Console.WriteLine($"b = {obj.b}");
+
+            //value null will be printed, so here we checking the null
+            if (obj.s == null)
+            {
+                Console.WriteLine("s = null");
+            }
+            Console.ReadKey();
+        }
+    }
+    class Test
+    {
+        public int i;
+        public bool b;
+        public string s;
+    }
+}
+
