6b94c7d2108b2216ffd86a6d49d9d6269ff3b53e
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-17 10:53:22 +0530

    Parameterized Constructors

---
+++ Types of Constructors/Example to understand Parameterized Constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_Constructors
+{
+    internal class Parameterized_Constructor
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ParameterizedConstructor obj = new ParameterizedConstructor(10);
+                Console.ReadKey();
+            }
+        }
+        public class ParameterizedConstructor
+        {
+            public ParameterizedConstructor(int i)
+            {
+                Console.WriteLine("Parameterized Constructor is called : " + i);
+            }
+        }
+          
+    }
+}
