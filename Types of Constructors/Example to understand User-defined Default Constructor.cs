7bc3dc8750f2594bdc041ecd0534cb01b443f5c7
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-17 10:56:22 +0530

    User-defined Default Constructor

---
+++ Types of Constructors/Example to understand User-defined Default Constructor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_Constructors
+{
+    internal class User_defined_Default_Constructor
+    {
+        class Employee
+        {
+            public int Id, Age;
+            public string Address, Name;
+            public bool IsPermanent;
+            //User Defined Default Constructor
+            public Employee()
+            {
+                Id = 101;
+                Age = 21;
+                Address = "Ghansoli";
+                Name = "Anjali";
+                IsPermanent = true;
+            }
+            public void Display()
+            {
+                Console.WriteLine("Employee Id is:  " + Id);
+                Console.WriteLine("Employee Age is:  " + Age);
+                Console.WriteLine("Employee Address is:  " + Address);
+                Console.WriteLine("Employee Name is:  " + Name);
+                Console.WriteLine("Is Employee Permanent:  " + IsPermanent);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee e1 = new Employee();
+                e1.Display();
+                Console.ReadKey();
+            }
+        }
+    }
+}
