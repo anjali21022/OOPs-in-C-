c16f5c70a20661a4b6a902a7914eb2340969638c
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-17 10:55:28 +0530

    Private Constructor

---
+++ Types of Constructors/Example to understand Private Constructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_Constructors
+{
+    class Program
+    {
+        private Program()
+        {
+            Console.WriteLine("This is private constructor");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Main method");
+            Console.ReadKey();
+        }
+    }
+}
