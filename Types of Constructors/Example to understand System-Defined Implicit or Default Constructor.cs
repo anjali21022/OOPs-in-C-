dcb08523ce07fbb10e475bc1a6513c33f32a9939
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-17 10:55:56 +0530

    Default Constructor

---
+++ Types of Constructors/Example to understand System-Defined Implicit or Default Constructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_Constructors
+{
+    internal class System_Defined_Implicit_or_Default_Constructor
+    {
+        class Employee
+        {
+            public int Id, Age;
+            public string Name, Address;
+            public bool IsPermanent;
+        }
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                Employee e1 = new Employee();
+                Console.WriteLine("Employee Id is : " + e1.Id);
+                Console.WriteLine("Employee Name is : " + e1.Name);
+                Console.WriteLine("Employee Age is : " + e1.Age);
+                Console.WriteLine("Employee Address is : " + e1.Address);
+                Console.WriteLine("Employee IsPermanent : " + e1.IsPermanent);
+                Console.ReadKey();
+            }
+        }
+    }
+}
