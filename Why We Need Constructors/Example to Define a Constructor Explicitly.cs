ba5af3c3bd4308b1b3267bf092256dafb80f2011
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-18 19:36:49 +0530

    Explicit Constructor

---
+++ Why We Need Constructors/Example to Define a Constructor Explicitly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Why_We_Need_Constructors
+{
+    internal class Define_a_Constructor_Explicitly
+    {
+        class First
+        {
+            public int x = 91;
+        }
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                First f1 = new First();
+                First f2 = new First();
+                First f3 = new First();
+                Console.WriteLine($"{f1.x} {f2.x} {f3.x}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
