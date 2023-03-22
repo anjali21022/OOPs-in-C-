d209a802efb2ce273913f123256b59b3f6addbce
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-22 11:58:01 +0530

    Example4

---
+++ Private Constructors/Creating an Instance from Outside the Class4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Constructor
+{
+    internal class Creating_an_Instance_from_Outside_the_Class4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"PI : {Test.PI}");
+            Console.WriteLine($"Square of 5 : {Test.GetSquare(5)}");
+            Console.ReadKey();
+        }
+    }
+    public static class Test
+    {
+        public static double PI = 3.14;
+        public static int GetSquare(int x)
+        {
+            return x * x;
+        }
+    }
+}
