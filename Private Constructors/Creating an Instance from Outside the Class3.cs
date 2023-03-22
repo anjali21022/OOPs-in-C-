b5d374cb0bb2cf678fbdb209b8f1d048da7760a8
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-22 11:57:39 +0530

    Example3

---
+++ Private Constructors/Creating an Instance from Outside the Class3.cs
@@ -0,0 +1,43 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Private_Constructors.Parents;
+
+namespace Private_Constructors
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            //Creating instance of Child class
+            Parentt.Child1 obj1 = new Parentt.Child1();
+            Child2 obj2 = new Child2();
+            Console.ReadKey();
+        }
+    }
+    public sealed class Parentt
+    {
+        //Private Constructor
+        private Parentt()
+        {
+            Console.WriteLine("Parent Class Private Constructor is Called");
+        }
+        *//*//public class Child1 : Parentt
+        {
+            public Child1()
+        {
+            Console.WriteLine("Child1 Class Public Constructor is Called");
+        }
+    }
+*//*}
+    *//*public class Child2 : Parentt
+    {
+        public Child2()
+        {
+            Console.WriteLine("Child2 Class Public Constructor is Called");
+        }
+    }*//*
+}
+*/
