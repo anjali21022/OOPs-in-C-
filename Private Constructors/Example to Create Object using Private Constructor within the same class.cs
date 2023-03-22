f0b7d37992eda2d188fbf688d32e82d3828fbf1a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-22 11:58:27 +0530

    Example5

---
+++ Private Constructors/Example to Create Object using Private Constructor within the same class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Types_of_Constructors;
+
+namespace Private_Constructors
+{
+    internal class Program
+    {
+        //Private Constructor
+        private Program()
+        {
+            Console.WriteLine("Private Constructor is called");
+        }
+        public void Method1()
+        {
+            Console.WriteLine("Method1 is called");
+        }
+        static void Main(string[] args)
+        {
+            //Creating instance of Program class using Private Constructor
+            Program obj = new Program();
+            obj.Method1();
+            Console.ReadKey();
+        }
+    }
+}
