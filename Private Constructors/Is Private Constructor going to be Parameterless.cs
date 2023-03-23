e20e964daae92251fa11d26878b83e5b0a0620e8
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-23 14:26:19 +0530

    Example7

---
+++ Private Constructors/Is Private Constructor going to be Parameterless.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PrivateConstructorDemo
+{
+    class Program
+    {
+        private Program()
+        {
+            Console.WriteLine("Private Parameterless Constructor is Called");
+        }
+        private Program(string message)
+        {
+            Console.WriteLine("Private Parameterized Constructor is Called");
+        }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            Program obj2 = new Program("Hello");
+            Console.ReadKey();
+        }
+    }
+}
