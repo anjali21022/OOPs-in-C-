2665c3da5763cf1b7048723a13a002c24419910a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-23 14:25:56 +0530

    Example6

---
+++ Private Constructors/Example to show Private Constructor Restricting Inheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Constructors
+{
+    internal class Private_Constructor_Restricting_Inheritance
+    {
+        static void Main(string[] args)
+        {
+            //Creating instance of Child class
+            Child obj = new Child();
+            Console.ReadKey();
+        }
+    }
+    public class Parent
+    {
+        //Private Constructor
+        private Parent()
+        {
+            Console.WriteLine("Parent Class Private Constructor is Called");
+        }
+        //Public Constructor
+        public Parent(string Message)
+        {
+            Console.WriteLine("Parent Class Public Constructor is Called");
+        }
+    }
+    public class Child : Parent
+    {
+        public Child() : base("Hello")
+        {
+            Console.WriteLine("Child Class Public Constructor is Called");
+        }
+    }
+}
