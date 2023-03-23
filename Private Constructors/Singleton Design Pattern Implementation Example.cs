d0514f1c9eae6dccb3b133bb93326a163fc68022
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-23 14:26:44 +0530

    Example8

---
+++ Private Constructors/Singleton Design Pattern Implementation Example.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Privatee_Constructors
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        private static readonly object Instancelock = new object();
+        public static Singleton GetSingletonInstance()
+        {
+            lock (Instancelock)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Singleton Constructor Executed {counter} Time");
+        }
+        public void SomeMethod(string Message)
+        {
+            Console.WriteLine($"Some Method Called : {Message}");            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromPlace1 = Singleton.GetSingletonInstance();
+            fromPlace1.SomeMethod("From Place 1");
+            Singleton fromPlace2 = Singleton.GetSingletonInstance();
+            fromPlace2.SomeMethod("From Place 2");
+            Console.ReadKey();
+        }
+    }
+}
