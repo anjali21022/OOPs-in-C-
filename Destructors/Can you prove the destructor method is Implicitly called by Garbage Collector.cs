7f18dd96346670eb716477b73d908b9a31bbca5e
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-24 18:22:39 +0530

    Example1

---
+++ Destructors/Can you prove the destructor method is Implicitly called by Garbage Collector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{
+    internal class DestructorDemo1
+    {
+        public DestructorDemo1()
+        {
+            Console.WriteLine("DestructorDemo Object Created");
+        }
+        ~DestructorDemo1()
+        {
+            Console.WriteLine("DestructorDemo Object Destroyed");
+        }
+    }
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Execution Started");
+            DestructorDemo1 demo = new DestructorDemo1();
+
+            //Making obj1 ready for Garbage Collection
+            demo = null;
+            GC.Collect();
+            Console.WriteLine("Some Statement Executed Inside Main Method");
+            demo = null;
+            GC.Collect();
+            Console.WriteLine("Some More Statement Executed Inside Main Method");
+            GC.Collect();
+            Console.WriteLine("Main Method Execution End");
+            Console.ReadKey();
+        }
+    }
+}
