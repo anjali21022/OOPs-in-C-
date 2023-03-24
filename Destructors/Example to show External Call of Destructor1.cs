8c533b646c727964126285344b80b4c71f354d7a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-24 18:23:28 +0530

    Example4

---
+++ Destructors/Example to show External Call of Destructor1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{
+    internal class External_Call_of_Destructor1
+    {
+        public External_Call_of_Destructor1()
+        {
+            Console.WriteLine("Constructor Object Created");
+        }
+        ~External_Call_of_Destructor1()
+        {
+            string type = GetType().Name;
+            Console.WriteLine($"Object {type} is Destroyed");
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            External_Call_of_Destructor1 obj1 = new External_Call_of_Destructor1();
+            External_Call_of_Destructor1 obj2 = new External_Call_of_Destructor1();
+            //Making obj1 for Garbage Collection
+            obj1 = null;
+            obj2 = null;
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
