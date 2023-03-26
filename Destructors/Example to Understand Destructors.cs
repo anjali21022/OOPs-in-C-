9bc462bfd112adbf6cef8da8aed267eb6fe2ae14
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-26 15:05:09 +0530

    Example4

---
+++ Destructors/Example to Understand Destructors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{
+    class DestructorDemo
+    {
+        public DestructorDemo()
+        {
+            Console.WriteLine("Constructor Object Created");
+        }
+        ~DestructorDemo()
+        {
+            string type = GetType().Name;
+            Console.WriteLine($"Object {type} is Destroyed");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DestructorDemo obj1 = new DestructorDemo();
+            DestructorDemo obj2 = new DestructorDemo();
+            //Making obj1 for Garbage Collection
+            obj1 = null;
+            Console.ReadKey();
+        }
+    }
+}
