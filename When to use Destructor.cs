531fa83a6951e8b3d92a40d72a668102dbcaf130
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-26 15:06:42 +0530

    Example7

---
+++ When to use Destructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{
+    public class UmmanagedResource
+    {
+        public UmmanagedResource()
+        {
+            //Creating Unmanaged Resources  
+            Console.WriteLine("Unmanaged Resource Created");
+        }
+        ~UmmanagedResource()
+        {
+            //Clean up Unmanaged Resources  
+            Console.WriteLine("Unmanaged Resource Destroyed");
+        }
+    }
+    class Program5
+    {
+        static void Main()
+        {
+            UmmanagedResource resource = new UmmanagedResource();
+            Console.WriteLine("Using Unmanaged Resource");
+            resource = null;
+            GC.Collect();
+
+            Console.ReadKey();
+        }
+    }
+}
