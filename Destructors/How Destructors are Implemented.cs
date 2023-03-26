7c53cf5a6ee1056a373a57aece9c02bc9a285620
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-26 15:06:18 +0530

    Example6

---
+++ Destructors/How Destructors are Implemented.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructors
+{   
+        public class First
+        {
+            ~First()
+            {
+                Console.WriteLine("Destructor of First Class");
+            }
+        }
+        public class Second : First
+        {
+            ~Second()
+            {
+                Console.WriteLine("Destructor of Second Class");
+            }
+        }
+        public class Third : Second
+        {
+            ~Third()
+            {
+                Console.WriteLine("Destructor of Third Class");
+            }
+        }    
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            Third obj = new Third();
+            obj = null;
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
