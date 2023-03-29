baa76640f60c384634a7a57f8683fc0c636f334e
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-29 10:38:10 +0530

    Encapsulation Principle1

---
+++ Encapsulation/Example to understand Encapsulation Principle1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class bank
+    {
+        private int Amount;
+        public int GetAmount()
+        {
+            return Amount;
+        }
+        public void SetAmount(int Amount)
+        {
+            if (Amount > 0)
+            {
+                this.Amount = Amount;
+            }
+            else
+            {
+                Console.WriteLine("Pass a Positive Value");
+            }
+        }
+    }
+    class Program2
+    {
+        public static void Main()
+        {
+            try
+            {
+                bank bank = new bank();
+                bank.SetAmount(10);
+                Console.WriteLine(bank.GetAmount());
+
+                bank.SetAmount(-110);
+                Console.WriteLine(bank.GetAmount());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
