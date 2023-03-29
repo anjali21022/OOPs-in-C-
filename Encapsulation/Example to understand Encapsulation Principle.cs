1fee1825abe732c8c27ccaeb585814425fa9a776
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-29 10:37:48 +0530

    Encapsulation Principle

---
+++ Encapsulation/Example to understand Encapsulation Principle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Banks
+    {
+        public int Amount;
+    }
+    class Program1
+    {
+        public static void Main()
+        {
+            Banks bank = new Banks();
+            //We can access the Amount Variable directly
+
+            //Setting positive amount
+            bank.Amount = 50;
+            Console.WriteLine(bank.Amount);
+
+            //Setting negative amount
+            bank.Amount = -50;
+            Console.WriteLine(bank.Amount);
+            Console.ReadKey();
+        }
+    }
+}
