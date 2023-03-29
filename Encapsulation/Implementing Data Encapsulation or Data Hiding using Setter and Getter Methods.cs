5ffd7c3305430e122e254d301f5515ed94fc64d6
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-03-29 10:38:58 +0530

    Data Hiding using Setter and Getter Methods

---
+++ Encapsulation/Implementing Data Encapsulation or Data Hiding using Setter and Getter Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Bank
+    {
+        //Hiding class data by declaring the variable as private
+        private double balance;
+
+        //Creating public Setter and Getter methods
+        //Public Getter Method
+        //This method is used to return the data stored in the balance variable
+        public double GetBalance()
+        {
+            //add validation logic if needed
+            return balance;
+        }
+
+        //Public Setter Method
+        //This method is used to stored the data  in the balance variable
+        public void SetBalance(double balance)
+        {
+            // add validation logic to check whether data is correct or not
+            this.balance = balance;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Bank bank = new Bank();
+            //You cannot access the Private Variable
+            //bank.balance; //Compile Time Error
+            //You can access the private variable via public setter and getter methods
+            bank.SetBalance(100);
+            Console.WriteLine(bank.GetBalance());
+            Console.ReadKey();
+        }        
+    }
+}
