58b46086db5dc49348a2c3526b60b69ee33065fc
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-03 10:35:35 +0530

    Example1

---
+++ Inheritance/Example to Understand Inheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Inheritance
+    {
+        class A
+        {
+            public void Method1()
+            {
+                Console.WriteLine("Method 1");
+            }
+            public void Method2()
+            {
+                Console.WriteLine("Method 2");
+            }
+        }
+        class B : A
+        {
+            static void Main()
+            {
+                B obj = new B();
+                obj.Method1();
+                obj.Method2();
+                Console.ReadKey();
+            }
+        }
+    }
+}
