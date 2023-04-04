093189808482cd01c44c652ad9fbc27a2ef0f83f
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-04 15:46:44 +0530

    Single Inheritance

---
+++ Example to Understand Single Inheritance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_of_Inheritance
+{
+    internal class Single_Inheritance
+    {
+        static void Main()
+        {
+            // Creating object of Child class and
+            // invoke the methods of Parent and Child classes
+            Cuboid obj = new Cuboid(2, 4, 6);
+            Console.WriteLine($"Volume is : {obj.Volume()}");
+            Console.WriteLine($"Area is : {obj.Area()}");
+            Console.WriteLine($"Perimeter is : {obj.Perimeter()}");
+            Console.ReadKey();
+        }
+    }
+    //Parent Class
+    public class Rectangle
+    {
+        public int length;
+        public int breadth;
+        public int Area()
+        {
+            return length * breadth;
+        }
+        public int Perimeter()
+        {
+            return 2 * (length + breadth);
+        }
+    }
+
+    //Child Class
+    public class Cuboid : Rectangle
+    {
+        public int height;
+        public Cuboid(int l, int b, int h)
+        {
+            height = h;
+            length = l;
+            breadth = b;
+        }
+        public int Volume()
+        {
+            return length * breadth * height;
+        }
+    }
+}
