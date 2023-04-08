db1ef67af9e517973bfdd1153e64226b906911eb
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-08 13:53:39 +0530

    Example2

---
+++ Generalization and Specialization/Example to Understand Specialization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalization_and_Specialization
+{
+    internal class Specialization
+    {
+        static void Main()
+        {
+            Cuboid obj = new Cuboid(7, 2, 5);
+            Console.WriteLine($"Area is {obj.Area()}");
+            Console.WriteLine($"Perimeter is {obj.Perimeter()}");
+            Console.WriteLine($"Volume is {obj.Volume()}");
+            Console.ReadKey();
+        }
+    }
+    class Rectangle
+    {
+        public int Length;
+        public int Breadth;
+        public int Area()
+        {
+            return Length * Breadth;
+        }
+        public int Perimeter()
+        {
+            return 2 * (Length + Breadth);
+        }
+    }
+    class Cuboid : Rectangle
+    {
+        public int Height;
+        public Cuboid(int l, int b, int h)
+        {
+            Length = l;
+            Breadth = b;
+            Height = h;
+        }
+        public int Volume()
+        {
+            return Length * Breadth * Height;
+        }
+    }
+}
