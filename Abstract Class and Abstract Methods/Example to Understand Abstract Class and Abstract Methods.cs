748033c03c741f926bfb2b0e3ae87b49ab2a5d12
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-09 13:29:57 +0530

    Example2

---
+++ Abstract Class and Abstract Methods/Example to Understand Abstract Class and Abstract Methods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_and_Abstract_Methods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            AbsChild abschild = new AbsChild();
+            abschild.Add(10, 20);
+            abschild.Sub(35, 12);
+            abschild.Mul(9, 8);
+            abschild.Div(25, 5);
+
+            Console.ReadKey();
+        }
+    }
+    public abstract class AbsParent
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine($"Addition of {x} and {y} is : {x + y}");
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+        }
+        public abstract void Mul(int x, int y);
+        public abstract void Div(int x, int y);
+    }
+    public class AbsChild : AbsParent
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
+        }
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+        }
+    }
+}
