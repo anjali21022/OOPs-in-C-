4334dfc704de9b142258f182eb19c04b22feb4cc
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-12 11:38:39 +0530

    Example2

---
+++ How to Use Abstract Classes and Methods in C Sharp Application/Example to Implement Abstract Classes and Abstract Methods in C Sharp Application Development.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How_to_Use_Abstract_Classes_and_Methods_in_C_Sharp_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 4);
+            Console.WriteLine($"Area of Rectangle : {rectangle.GetArea()}");
+
+            Circle circle = new Circle(12);
+            Console.WriteLine($"Area of Circle : {circle.GetArea()}");
+
+            Triangle triangle = new Triangle(15, 12);
+            Console.WriteLine($"Area of Triangle : {triangle.GetArea()}");
+
+            Cone cone = new Cone(32, 12);
+            Console.WriteLine($"Area of Cone : {cone.GetArea()}");
+
+            Console.ReadKey();
+        }
+    }
+    public abstract class Shape
+    {
+        public double Height;
+        public double Width;
+        public double Radius;
+        public const float PI = 3.14f;
+        public abstract double GetArea();
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle (double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width= Width;
+        }
+        public override double GetArea()
+        {
+            return Height * Width;
+        }
+    }
+    public class Circle : Shape
+    {
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public override double GetArea()
+        {
+            return PI * Radius * Radius;
+        }
+    }
+    public class Triangle : Shape
+    {
+        public Triangle(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+        public override double GetArea()
+        {
+            return (Height * Width)/ 2;
+        }
+    }
+    public class Cone : Shape
+    {
+        public Cone(double Radius, double Height)
+        {
+            this.Radius = Radius;
+            this.Height = Height;
+        }
+        public override double GetArea()
+        {
+            return PI * Radius * (Radius + Math.Sqrt(Height * Height + Radius * Radius));
+        }
+    }
+}
