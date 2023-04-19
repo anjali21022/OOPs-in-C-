9aac3ca742ba3dc4d761af534e3afc0a4ae86e14
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-19 10:39:01 +0530

    Multiple Inheritances with Interfaces

---
+++ Multiple Inheritance/Example to Understand Multiple Inheritances with Interfaces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Inheritance
+{
+    class Multiple_Inheritances_with_Interfaces
+    {
+        static void Main(string[] args)
+        {
+            MultipleInheritanceTest obj = new MultipleInheritanceTest();
+            obj.Test();
+            Console.ReadKey();
+        }
+    }
+    public interface Interface1
+    {
+        void Test();
+    }
+    public interface Interface2
+    {
+        void Test();
+    }
+    public class MultipleInheritanceTest : Interface1, Interface2
+    {
+        public void Test()
+        {
+            Console.WriteLine("Test Method is implemented in Child Class");
+        }
+    }
+}
