32097609ecae25c549486605e49610efca091139
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-19 10:39:36 +0530

    Implement Each Interfaces Method Separately

---
+++ Multiple Inheritance/How to Implement Each Interfaces Method Separately.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Inheritancee
+{
+    class How_to_Implement_Each_Interfaces_Method_Separately
+    {
+        static void Main(string[] args)
+        {
+            MultipleInheritanceTest obj = new MultipleInheritanceTest();
+            obj.Test();
+            //You cannot call the Show method using obj
+            //obj.Show();
+            //Using Interface Reference call the Show method
+            Interface1 i1 = obj;
+            i1.Show();
+            //Typecase the object to interface type and call the show method
+            ((Interface2)obj).Show();
+            Console.ReadKey();
+        }
+    }
+    public interface Interface1
+    {
+        void Test();
+        void Show();
+    }
+    public interface Interface2
+    {
+        void Test();
+        void Show();
+    }
+    public class MultipleInheritanceTest : Interface1, Interface2
+    {
+        //Normal Implementation
+        public void Test()
+        {
+            Console.WriteLine("Test Method is Implemented in Child Class");
+        }
+        //Explicit Interface Implementation
+        void Interface1.Show()
+        {
+            Console.WriteLine("Interface1 Show Method is Implemented in Child Class");
+        }
+        //Explicit Interface Implementation
+        void Interface2.Show()
+        {
+            Console.WriteLine("Interface2 Show Method is Implemented in Child Class");
+        }
+    }
+}
