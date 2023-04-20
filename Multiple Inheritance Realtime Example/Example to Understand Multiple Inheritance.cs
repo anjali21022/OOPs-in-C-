b39a3172276edbca03b078dfddf50a4ecdac907a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-20 10:49:50 +0530

    Example

---
+++ Multiple Inheritance Realtime Example/Example to Understand Multiple Inheritance.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Inheritance_Realtime_Example
+{
+    class Multiple_Inheritance
+    {
+        static void Main(string[] args)
+        {
+            HPLaserJetPrinter hPLaserJetPrinter = new HPLaserJetPrinter();
+            hPLaserJetPrinter.Scan("Scan Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Print("Print Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Fax("Fax Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.PrintDuplex("Print Duplex Services by HPLaserJetPrinter");
+
+            LiquidInkjetPrinter liquidInkjetPrinter = new LiquidInkjetPrinter();
+            liquidInkjetPrinter.Scan("Scan Services by LiquidInkjetPrinter");
+            liquidInkjetPrinter.Print("Print Services by LiquidInkjetPrinter");
+            //Fax and PrintDuplex are not available in LiquidInkjetPrinter
+            //liquidInkjetPrinter.Fax("Fax Services");
+            //liquidInkjetPrinter.PrintDuplex("Print Duplex Services");
+            Console.Read();
+        }
+    }
+    public interface IPrinterTasks
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+    }
+    interface IFaxTasks
+    {
+        void Fax(string content);
+    }
+    interface IPrintDuplexTasks
+    {
+        void PrintDuplex(string content);
+    }
+    public class HPLaserJetPrinter : IPrinterTasks, IFaxTasks, IPrintDuplexTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine(PrintContent);
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine(ScanContent);
+        }
+        public void Fax(string FaxContent)
+        {
+            Console.WriteLine(FaxContent);
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            Console.WriteLine(PrintDuplexContent);
+        }
+    }
+    class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine(PrintContent);
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine(ScanContent);
+        }
+    }
+}
