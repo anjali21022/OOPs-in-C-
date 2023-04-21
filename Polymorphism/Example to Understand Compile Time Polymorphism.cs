f01121454219b990d2d11e04869e58f04e18ce30
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-21 10:40:37 +0530

    Compile Time Polymorphism

---
+++ Polymorphism/Example to Understand Compile Time Polymorphism.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+     class Program
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Add(string s1, string s2)
+        {
+            Console.WriteLine(s1 + " " + s2);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Add(10, 20);
+            obj.Add(12.3f, 14.8f);
+            obj.Add("Anjali", "Chavan");
+            Console.ReadKey();
+        }
+    }
+}
