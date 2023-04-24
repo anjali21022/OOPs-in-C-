d6b05d4f0cb4af871bf72ed8d3b2eec6bd56a449
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-24 10:18:12 +0530

    Constructor Method Overloading

---
+++ Method Overloading/Example to Understand Constructor Method Overloading.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class ConstructorOverloading
+    {
+        int x, y, z;
+        public ConstructorOverloading(int x)
+        {
+            Console.WriteLine("Constructor1 Called");
+            this.x = x;
+        }
+        public ConstructorOverloading(int x, int y)
+        {
+            Console.WriteLine("Constructor2 Called");
+            this.x = x;
+            this.y = y;
+        }
+        public ConstructorOverloading(int x, int y, int z)
+        {
+            Console.WriteLine("Constructor3 Called");
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"X={x}, Y={y}, Z={z}");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            ConstructorOverloading obj1 = new ConstructorOverloading(10);
+            obj1.Display();
+            ConstructorOverloading obj2 = new ConstructorOverloading(10, 20);
+            obj2.Display();
+            ConstructorOverloading obj3 = new ConstructorOverloading(10, 20, 30);
+            obj3.Display();
+
+            Console.ReadKey();
+        }        
+    }
+}
