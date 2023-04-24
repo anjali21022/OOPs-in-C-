966404a0e71941aa24c46c15d1021558739d4b2a
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-24 10:18:45 +0530

    Inheritance-Based Method Overloading

---
+++ Method Overloading/Example to understand Inheritance-Based Method Overloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class Class1
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+    class Class2 : Class1
+    {
+        public void Add(string s1, string s2)
+        {
+            Console.WriteLine(s1+ " "  +s2);
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.Add(10, 20);
+            obj.Add(10.5f, 20.7f);
+            obj.Add("Anjali", "Chavan");
+            Console.ReadKey();
+        }
+    }
+}
