98d57b30307f87de3c028ffb78c4e96a5f57a1a0
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-25 10:46:44 +0530

    Method Overloading1

---
+++ Method Overloading/Example to Understand Method Overloading1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class Program1
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Add(string s1, string s2)
+        {
+            Console.WriteLine(s1 +" "+ s2);
+        }
+        
+        static void Main(string[] args)
+        {
+            Program1 obj = new Program1();
+            obj.Add(2, 5);
+            obj.Add(3.1f, 5.0f);
+            obj.Add("Anjali", "Chavan");
+            Console.ReadKey();
+        }
+    }
+}
