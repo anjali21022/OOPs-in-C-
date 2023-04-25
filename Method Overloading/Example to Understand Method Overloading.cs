a793e97794502341b597a898d5850967867d4c86
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-25 10:45:37 +0530

    Method Overloading

---
+++ Method Overloading/Example to Understand Method Overloading.cs
@@ -0,0 +1,44 @@
+﻿using Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Method(); //Invoke the 1st Method
+            obj.Method(10); //Invoke the 2nd Method
+            obj.Method("Anjali"); //Invoke the 3rd Method
+            obj.Method(1, "Anjali"); //Invoke the 4th Method
+            obj.Method("Anjali", 01); //Invoke the 5th Method
+
+            Console.ReadKey();
+        }
+        public void Method()
+        {
+            Console.WriteLine("1st Method");
+        }
+        public void Method(int i)
+        {
+            Console.WriteLine("2nd Method");
+        }
+        public void Method(string s)
+        {
+            Console.WriteLine("3rd Methods");
+        }
+        public void Method(int i, string s)
+        {
+            Console.WriteLine("4th Method");
+        }
+        public void Method(string s, int i)
+        {
+            Console.WriteLine("5th Method");
+        }
+    }
+}
