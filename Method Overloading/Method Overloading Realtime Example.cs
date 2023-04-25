3c85612ebd07db58dfe7f21eb65ed39e04eeb526
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-25 10:48:03 +0530

    Realtime Example

---
+++ Method Overloading/Method Overloading Realtime Example.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Logger
+    {
+        public static void Log(string ClassName, string MethodName, string Message)
+        {
+            Console.WriteLine($"DateTime: {DateTime.Now.ToString()}, ClassName: {ClassName}, MethodName:{MethodName}, Message:{Message}");
+        }
+        public static void Log(string uniqueId, string ClassName, string MethodName, string Message)
+        {
+            Console.WriteLine($"DateTime: {DateTime.Now.ToString()}, UniqueId: {uniqueId}, ClassName: {ClassName}, MethodName:{MethodName}, Message:{Message}");
+        }
+        public static void Log(string Message)
+        {
+            Console.WriteLine($"DateTime: {DateTime.Now.ToString()}, Message: {Message}");
+        }
+        public static void Log(string ClassName, string MethodName, Exception ex)
+        {
+            Console.WriteLine($"DateTime: {DateTime.Now.ToString()}, ClassName: {ClassName}, MethodName:{MethodName}, Exception Message:{ex.Message}, \nException StackTrace: {ex.StackTrace}");
+        }
+        //You create many overloaded versions as per your business requirements
+    }
+
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            string ClassName = "Program";
+            string MethodName = "Main";
+            string UniqueId = Guid.NewGuid().ToString();
+            Logger.Log(ClassName, MethodName, "Message 1");
+            Logger.Log(UniqueId, ClassName, MethodName, "Message 2");
+            Logger.Log("Message 3");
+            try
+            {
+                int Num1 = 10, Num2 = 0;
+                int result = Num1 / Num2;
+                Logger.Log(UniqueId, ClassName, MethodName, "Message 4");
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ClassName, MethodName, ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
