278e3be85293a413c5ac760a0cce525c3a6e4a35
Anjali Chavan <121797940+anjali21022@users.noreply.github.com>
2023-04-27 10:19:27 +0530

    Example2

---
+++ Operator Overloading/Example to add two Complex Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(3, 5);
+            c1.Display();
+            Complex c2 = new Complex(2, 9);
+            c2.Display();
+            Complex c3 = Complex.Add(c1, c2);
+            c3.Display();
+
+            Console.ReadKey();
+        }
+    }
+    public class Complex
+    {
+        private int real;
+        private int img;
+        public Complex(int r = 0, int i = 0)
+        {
+            real = r;
+            img = i;
+        }
+        public static Complex Add(Complex c1, Complex c2)
+        {
+            Complex temp = new Complex();
+            temp.real = c1.real + c2.real;
+            temp.img = c1.img + c2.img;
+            return temp;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{real} + i{img}");
+        }
+    }
+}
